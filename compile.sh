14e2d02eeef7362819df1065461e336c7e951978
tjysdsg <tjy1018543509@163.com>
2021-07-31 22:54:43 +0800

    Try to fix the problem that clang libs aren't installed

--- compile.sh
+++ compile.sh
@@ -19,8 +19,8 @@ export CXX=$(which clang++)
 # https://llvm.org/docs/CMake.html#llvm-related-variables
 cmake ../llvm -G Ninja -DCMAKE_BUILD_TYPE=RelWithDebInfo -DLLVM_INCLUDE_EXAMPLES=OFF -DLLVM_INCLUDE_TESTS=OFF \
   -DLLVM_INCLUDE_BENCHMARKS=OFF -DLLVM_BUILD_LLVM_DYLIB=OFF -DLLVM_ENABLE_BINDINGS=OFF \
-  -DLLVM_ENABLE_PROJECTS="llvm;clang" \
-  -DLLVM_ENABLE_LLD=ON -DLLVM_PARALLEL_LINK_JOBS=1 -DLLVM_INCLUDE_TOOLS=OFF \
+  -DLLVM_ENABLE_PROJECTS="llvm;clang;clang-tools-extra" \
+  -DLLVM_ENABLE_LLD=ON -DLLVM_PARALLEL_LINK_JOBS=1 \
   -DCMAKE_INSTALL_PREFIX=${root_dir}/llvm-10
 
 cmake --build . --target install --config Release
